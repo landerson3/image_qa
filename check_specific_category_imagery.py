b94edfb41fd9ab19497bb7c13451035b000cd7d3
Luke Anderson <landerson2@restorationhardware.com>
2024-05-14 11:23:05 -0700

    merge to production

--- check_specific_category_imagery.py
+++ check_specific_category_imagery.py
@@ -7,7 +7,7 @@ from box_api import box_api_class
 output_path = os.path.expanduser(f'~/Desktop/product_image_check.csv')
 if os.path.exists(output_path): os.remove(output_path)
 rh = rh_atg_api.rh_atg_wrapper(target = 'b')
-rh.product_image_check('cat1537037')
+rh.product_image_check('cat30530003')
 # box = box_api_class.box_api()
 # t = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 # new_path = os.path.expanduser(f'~/Desktop/product_image_check_{t}.csv')
